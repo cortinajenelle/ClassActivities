ac48aa614135d43b65a946f4aafcb24697bdbe84
Cortina Caldwell <cortinajenellecaldwell@gmail.com>
2023-06-26 20:37:15 -0400

    Function Invocation

---
+++ Week3/3.1.1-FunctionDeclaration.js
@@ -0,0 +1,41 @@
+// TODO: using the function expressions below, refactor them into function declarations
+// #1 function expression
+const greet = function() {
+  console.log("Hello!");
+};
+
+// #1 function declaration
+function greet (){
+  console.log("Hello!");
+}
+
+// #2 function expression
+const threeModTwo = function() {
+  console.log(3 % 2);
+};
+
+// #2 function declaration
+function threeModTwo (){
+console.log(3 % 2);
+}
+
+// #3 function expression
+let age = 18;
+const checkDrivingAge = function() {
+  if (age >= 16) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+};
+
+// #3 function declaration
+function checkDrivingAge (){
+  if (age >= 16) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+
+

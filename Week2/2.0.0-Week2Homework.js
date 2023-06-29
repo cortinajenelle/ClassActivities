48fff04ff9a00b727ea6d6b05729cb84ffa0687b
Cortina Caldwell <cortinajenellecaldwell@gmail.com>
2023-06-29 13:44:49 -0400

    Week 2 and 3

--- Week2/2.0.0-Week2Homework.js
+++ Week2/2.0.0-Week2Homework.js
@@ -2,22 +2,16 @@
 // Have the variables reference the name of your Pizza Place and the number of toppings your Pizza Place offers.
 
 const pizzaPlace = "Lupis Pizza";
-const numberOfToppings = 7;
+let numberOfToppings = 7;
 
 // Print the variables and their types.
 
-console.log(typeof string);
-console.log(typeof number);
+console.log(pizzaPlace);
+console.log(numberofToppings);
 
 // Print a template literal that uses both variables to construct a short sentence about your Pizza Place.
 
-console.log(
-  "When you order at" +
-    { pizzaPlace } +
-    ", they give you the option of " +
-    { numberOfToppings } +
-    "toppings. "
-);
+console.log("When you order at" + { pizzaPlace } + ", they give you the option of " + { numberOfToppings } + "toppings. ");
 
 // Construct an if statement that prints "Quality, not quantity." if you offer less than 10 toppings,
 // or "A whole lot of pizza." if you offer 10 or more toppings.

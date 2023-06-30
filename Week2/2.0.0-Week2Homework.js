a383b11aec1e4e12ef28ab16db4c56601cebf1a4
Cortina Caldwell <cortinajenellecaldwell@gmail.com>
2023-06-29 20:13:16 -0400

    Week 2 Homework

--- Week2/2.0.0-Week2Homework.js
+++ Week2/2.0.0-Week2Homework.js
@@ -1,24 +1,28 @@
 // Inside your JS file, create two variables: pizzaPlace and numberOfToppings
 // Have the variables reference the name of your Pizza Place and the number of toppings your Pizza Place offers.
 
-const pizzaPlace = "Lupis Pizza";
+let pizzaPlace = "Lupis Pizza";
 let numberOfToppings = 7;
 
 // Print the variables and their types.
 
 console.log(pizzaPlace);
+console.log(typeof pizzaPlace);
 console.log(numberofToppings);
+console.log(typeof numberofToppings);
 
 // Print a template literal that uses both variables to construct a short sentence about your Pizza Place.
 
-console.log('"When you order at" + { pizzaPlace } + ", they give you the option of " + { numberOfToppings } + "toppings. "'git );
+console.log(
+  `"When you order at·${pizzaPlace} they give you the option of ${numberOfToppings}·toppings."`
+);
 
 // Construct an if statement that prints "Quality, not quantity." if you offer less than 10 toppings,
 // or "A whole lot of pizza." if you offer 10 or more toppings.
 
 if (numberOfToppings < 10) {
   console.log("Quality, not quantity.");
-} else if (numberofToppings > 10) {
+} else if (numberofToppings >= 10) {
   console.log("A whole lot of pizza.");
 }
 
@@ -30,4 +34,10 @@ while (x <= 10) {
   numberofToppings = x;
   console.log(numberofToppings);
   x++;
+}
+
+for (let x = 1; x <= numberOfToppings; x++) {
+  if (x % 2 === 0) {
+    console.log(x);
+  }
 }
